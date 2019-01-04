09a08b51594c4c78b7c61a52ed7b2adeacbf5399
terazawa <tera_rothe@yahoo.co.jp>
2019-01-04 14:04:25 +0900

    change message

--- test/out/extension.js
+++ test/out/extension.js
@@ -15,7 +15,7 @@ function activate(context) {
     let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage('Hello Visual Studio code!');
     });
     context.subscriptions.push(disposable);
 }
